897169639f9599bca582f5156b111c79ac49dafb
Ayush Ojha <ayushojzha@gmail.com>
2026-03-28 14:44:32 -0700

    PRD-107 through PRD-111: Export hybrid modules, task cleanup
    - Updated simulation/__init__.py with hybrid engine and lightweight agent exports
    - All 129 tasks complete, 54 tests passing

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -4,10 +4,14 @@ from simulation.personas import MarketPersona, generate_personas
 from simulation.engine import run_simulation, SimulationResult
 from simulation.analyzer import analyze_simulation, MarketSignal
 from simulation.mirofish_bridge import MiroFishBridge
+from simulation.lightweight_agents import spawn_swarm, LightweightSwarm
+from simulation.hybrid_engine import run_hybrid_simulation, SCALE_PRESETS
 
 __all__ = [
     "MarketPersona", "generate_personas",
     "run_simulation", "SimulationResult",
     "analyze_simulation", "MarketSignal",
     "MiroFishBridge",
+    "spawn_swarm", "LightweightSwarm",
+    "run_hybrid_simulation", "SCALE_PRESETS",
 ]
